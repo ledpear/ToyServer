336128fc9b218a6fd4f8641c98aadb20c54c7f43
LEDPEAR <tjsrb75@gmail.com>
2023-07-28 22:29:25 +0900

    Create ToyServer

---
+++ Source/ToyServer/ToyServerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToyServerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOYSERVER_API AToyServerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
